9a8c47fc8064cb9a15377e2931265fa273e5850f
Noa Sillam <noasillam@gmail.com>
2024-10-31 15:15:21 +0100

    commit-design

---
+++ lib/bs/src/components/HelloWorld.d
@@ -0,0 +1,1 @@
+src/components/HelloWorld.cmj : src/components/Footer.cmj src/components/Footer.cmi src/components/Header.cmj src/components/Header.cmi
---
+++ lib/bs/src/components/MarqueList.d
@@ -0,0 +1,1 @@
+src/components/MarqueList.cmj : src/components/Api.cmj src/components/Api.cmi src/components/Header.cmj src/components/Header.cmi
